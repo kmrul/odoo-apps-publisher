8c676607aa9f0ece06fb25b9efbe2dbef2b9c2a7
kamrul <khasancsit@gmail.com>
2020-04-14 15:32:26 +0600

    sales order report

---
+++ sales-order-report/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Sales Order Report',
+    'version': '12.0.1.0.1',
+    'summary': 'Show your sales order reports',
+    'author': 'Kamrul Hasan',
+    'maintainer':'Kamrul Hasan',
+    'Company': 'ViewPoints Ltd.',
+    'website':'http://kamrul.net',
+    'depends': ['sale_management'],
+    'license': 'LGPL-3',
+    'category': 'Sales',
+    'data':[
+        'datas/data.xml',
+        'wizards/sales_order_report_wizard.xml',
+        'reports/sales_order_report.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'price': 0.0,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False
+}
